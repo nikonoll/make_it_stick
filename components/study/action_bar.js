80f36703b01b5c72a6a0b567beac35fe7fbd6c6c
Niko <dernikonoll@gmail.com>
2020-07-09 17:31:05 +0200

    flexboxing those buttons

--- components/study/action_bar.js
+++ components/study/action_bar.js
@@ -21,28 +21,23 @@ class ActionBar extends React.Component {
         if (this.props.learned !== "") {
             return (
 
-                <div className="flex-shrink-0 flex items-center justify-between flex-wrap bg-gray-200 p-3">
-                    <Link href={`/deck/${this.props.deckId}/study`}><button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">next card</button></Link>
+                <div className="flex-shrink-0 flex items-center justify-between flex-wrap p-3">
+                    <Link href={`/deck/${this.props.deckId}/study`}><button className="flex-1 bg-green-400 hover:bg-green-500 text-white font-bold uppercase py-2 px-4">next card</button></Link>
                 </div>
             )
         }
         if (this.props.flipped) {
             return (
-                <div className="flex-shrink-0 flex items-center justify-between flex-wrap bg-gray-200 p-3">
-                    <div className="">
-                        <p className="base-text mb-2">How was that?</p>
-                        <button data-txt="easy" onClick={this.clickLearned} className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">easy</button>
-                        <button data-txt="ok" onClick={this.clickLearned} className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">ok</button>
-                        <button data-txt="hard" onClick={this.clickLearned} className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">hard</button>
-                    </div>
+                <div className="flex-shrink-0 flex items-center justify-between flex-wrap p-3">
+                    <button data-txt="easy" onClick={this.clickLearned} className="flex-1 bg-green-400 hover:bg-green-500 text-white font-bold uppercase py-2 px-4 mr-4">easy</button>
+                    <button data-txt="ok" onClick={this.clickLearned} className="flex-1 bg-green-400 hover:bg-green-500 text-white font-bold uppercase py-2 px-4 mr-4">ok</button>
+                    <button data-txt="hard" onClick={this.clickLearned} className="flex-1 bg-green-400 hover:bg-green-500 text-white font-bold uppercase py-2 px-4">hard</button>
                 </div>
             )
         }
         return (
-            <div className="flex-shrink-0 flex items-center justify-between flex-wrap bg-gray-200 p-3">
-                <div>
-                    <button onClick={this.clickFlip} className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Flip</button>
-                </div>
+            <div className="flex-shrink-0 flex items-center justify-between flex-wrap p-3">
+                <button onClick={this.clickFlip} className="flex-1 bg-green-400 hover:bg-green-500 text-white font-bold uppercase py-2 px-4">Flip</button>
             </div>
         )
     }

a239fa39337a3ff279a94c80fa0109735758cf95
Niko Noll <niko.noll@xing.com>
2020-07-24 19:28:04 +0200

    queue

--- tests/queue.test.js
+++ tests/queue.test.js
@@ -1,8 +1,8 @@
-const scoreCalc = require('../lib/score_calc')
+const queue = require('../lib/queue')
 
-describe('wip', () => {
-  test('gives back delta of +10 when answer ok', () => {
-    expect(scoreCalc.scoreDelta("ok")).toBe(10);
+describe('queue', () => {
+  test('gives back a good study order', () => {
+    expect(queue.buildQueue([{cardId: 1, score: 20}, {cardId: 2, score: 10}])).toBe([2,1]);
   });
 })
 
